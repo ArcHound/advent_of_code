822a3307f3dc6565ac0bedee5f2681a4029446e1
ArcHound <miloslav.homer@kiwi.com>
2023-12-05 07:59:26 +0100

    feat:day3, day4

--- aoc/year2023/day3.py
+++ aoc/year2023/day3.py
@@ -1,10 +1,65 @@
 # 2023-3
 import logging
+from lib import vector2d
 
 log = logging.getLogger("aoc_logger")
 
+def parse(in_data):
+    state = 'blank'
+    num = '' 
+    num_start = 0
+    symbols = list()
+    numbers = list()
+    c_line = 0
+    for line in in_data.splitlines():
+        c_char = 0
+        for c in line:
+            if c in '0123456789' and (state=='blank' or state=='num'):
+                num += c
+                if state=='blank':
+                    num_start=c_char
+                state = 'num'
+            elif c == '.' and state=='num':
+                state = 'blank'
+                numbers.append({'points':[(num_start+i, c_line) for i in range(len(num))], 'val':int(num)})
+                num = ''
+            elif c not in '0123456789.' and state=='blank':
+                symbols.append({'point':(c_char,c_line), 'val':c})
+                state = 'blank'
+            elif c not in '0123456789.' and state=='num':
+                symbols.append({'point':(c_char,c_line), 'val':c})
+                numbers.append({'points':[(num_start+i, c_line) for i in range(len(num))], 'val':int(num)})
+                num = ''
+                state = 'blank'
+            else:
+                pass
+            c_char +=1
+        if state == 'num':
+            numbers.append({'t':'num','points':[(num_start+i, c_line) for i in range(len(num))], 'len':len(num), 'val':int(num)})
+            num = ''
+            state = 'blank'
+        c_line += 1
+    return numbers, symbols 
+
 def part1(in_data):
-    return "part1 output 2023-3"
+    numbers, symbols = parse(in_data)
+    count = 0
+    log.debug(numbers)
+    for sym in symbols:
+        count += sum([num['val'] for num in numbers if any([vector2d.v_nearbysquare(sym['point'],point) for point in num['points']])])
+        log.debug(sym)
+        log.debug([num['val'] for num in numbers if any([vector2d.v_nearbysquare(sym['point'],point) for point in num['points']])])
+        log.debug('--------')
+    return count
 
 def part2(in_data):
-    return "part2 output 2023-3"
+    numbers, symbols = parse(in_data)
+    count = 0
+    log.debug(numbers)
+    for sym in symbols:
+        if sym['val']!='*':
+            continue
+        nums = [num['val'] for num in numbers if any([vector2d.v_nearbysquare(sym['point'],point) for point in num['points']])]
+        if len(nums) == 2:
+            count += nums[0]*nums[1]
+    return count
--- tests/year2023/test_year2023_day3.py
+++ tests/year2023/test_year2023_day3.py
@@ -4,15 +4,24 @@ import pytest
 
 from aoc.year2023.day3 import part1, part2
 
-in_data1 = """
+in_data1 = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
 """
-part1_ans = "part1 output 2023-3"
+part1_ans = "4361"
 
 def test_part1():
     assert str(part1(in_data1)) == part1_ans
 
 in_data2 = in_data1
-part2_ans = "part2 output 2023-3"
+part2_ans = "467835"
 
 def test_part2():
     assert str(part2(in_data2)) == part2_ans

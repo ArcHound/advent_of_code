397edf6c7106000cc36c6523358f76bf10804e10
ArcHound <miloslav.homer@kiwi.com>
2023-12-08 17:41:00 +0100

    feat: 2022, day10

--- aoc/year2022/day10.py
+++ aoc/year2022/day10.py
@@ -4,9 +4,73 @@ import logging
 log = logging.getLogger("aoc_logger")
 
 
+def process_instructions(instructions, mod, rem):
+    cycle = 0
+    x = 1
+    profile = list()
+    for ins in instructions:
+        if ins.split()[0] == "noop":
+            cycle += 1
+            if cycle % mod == rem:
+                log.debug(f"{cycle}, {x}")
+                profile.append(cycle * x)
+        elif ins.split()[0] == "addx":
+            cycle += 1
+            if cycle % mod == rem:
+                log.debug(f"{cycle}, {x}")
+                profile.append(cycle * x)
+            cycle += 1
+            val = int(ins.split()[1])
+            if cycle % mod == rem:
+                log.debug(f"{cycle}, {x}")
+                profile.append(cycle * x)
+            x += val
+    return sum(profile)
+
+
+def draw(cycle, x, buf):
+    sprite = [x - 1, x, x + 1]
+    log.debug(f"Cycle: {cycle}")
+    log.debug(f"Sprite: {sprite}")
+    if (cycle - 1) % 40 in sprite:
+        buf += "#"
+        log.debug("#")
+    else:
+        buf += "."
+        log.debug(".")
+    log.debug("----------")
+    return buf
+
+
+def render(buf):
+    buf2 = [buf[i : i + 40] for i in range(0, len(buf), 40)]
+    for b in buf2:
+        log.debug(b)
+    return "\n".join(buf2)
+
+
+def process_instructions_2(instructions, mod, rem):
+    cycle = 0
+    x = 1
+    buf = ""
+    profile = list()
+    for ins in instructions:
+        if ins.split()[0] == "noop":
+            cycle += 1
+            buf = draw(cycle, x, buf)
+        elif ins.split()[0] == "addx":
+            cycle += 1
+            buf = draw(cycle, x, buf)
+            cycle += 1
+            val = int(ins.split()[1])
+            buf = draw(cycle, x, buf)
+            x += val
+    return render(buf)
+
+
 def part1(in_data):
-    return "part1 output 2022-10"
+    return process_instructions(in_data.splitlines(), 40, 20)
 
 
 def part2(in_data):
-    return "part2 output 2022-10"
+    return process_instructions_2(in_data.splitlines(), 40, 20)
--- tests/year2022/test_year2022_day10.py
+++ tests/year2022/test_year2022_day10.py
@@ -4,16 +4,168 @@ import pytest
 
 from aoc.year2022.day10 import part1, part2
 
-in_data = """
+in_data1 = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
 """
-
-part1_ans = "part1 output 2022-10"
-part2_ans = "part2 output 2022-10"
+part1_ans = "13140"
 
 
 def test_part1():
-    assert str(part1(in_data)) == part1_ans
+    assert str(part1(in_data1)) == part1_ans
+
+
+in_data2 = in_data1
+part2_ans = """##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
 
 
 def test_part2():
-    assert str(part2(in_data)) == part2_ans
+    assert str(part2(in_data2)) == part2_ans

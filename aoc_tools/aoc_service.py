ed1c187f265d432cfed3280aaec7bb0c2fc62f36
Miloslav Homer <miloslav.homer@gmail.com>
2024-10-09 18:38:04 +0200

    feat: private leaderboard for aoc_service

--- aoc_tools/aoc_service.py
+++ aoc_tools/aoc_service.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import dataclasses
 import re
 import requests
 import requests_cache
@@ -8,6 +9,14 @@ from bs4 import BeautifulSoup
 import logging
 
 log = logging.getLogger("aoc_logger")
+
+
+@dataclasses.dataclass
+class PrivateLeaderboardEntry:
+    name: str
+    stars: int
+    points: int
+    completed: list
 
 
 class AOC_Service:
@@ -30,18 +39,52 @@ class AOC_Service:
         resp = self.aoc_session.get(f"{self.aoc_url}/{year}/day/{day}/input")
         return resp.text
 
+    def parse_user(self, text):
+        soup = BeautifulSoup(text, "html.parser")
+        user = soup.find("div", {"class": "user"})
+        return user.text
+
+    def get_user(self):
+        r = self.aoc_session.get(f"{self.aoc_url}/")
+        user = self.parse_user(r.text)
+        return user
+
+    def parse_private_leaderboards(self, text):
+        soup = BeautifulSoup(text, "html.parser")
+        links = soup.find("article").find("div").find_all("a")
+        return [l["href"].split("/")[-1] for l in links]
+
+    def get_private_leaderboards(self):
+        resp = self.aoc_session.get(f"{self.aoc_url}/leaderboard/private")
+        leaderboards = self.parse_private_leaderboards(resp.text)
+        return leaderboards
+
+    def get_private_leaderboard(self, year, leaderboard_id):
+        resp = self.aoc_session.get(
+            f"{self.aoc_url}/{year}/leaderboard/private/view/{leaderboard_id}.json"
+        ).json()
+        leaderboard = list()
+        for entry, value in resp.get("members", dict()).items():
+            completions = [
+                (int(x), len(value["completion_day_level"][x]))
+                for x in value["completion_day_level"]
+            ]
+            lb_entry = PrivateLeaderboardEntry(
+                name=value["name"],
+                stars=value["stars"],
+                points=value["local_score"],
+                completed=completions,
+            )
+            leaderboard.append(lb_entry)
+        return leaderboard
+
+    def get_global_leaderboard(self, year):
+        pass
+
     def parse_stars(self, text):
         pattern = "Your puzzle answer was"
         stars = len(re.findall(pattern, text))
         return stars
-
-    def parse_solution_result(self, text):
-        soup = BeautifulSoup(text, "html.parser")
-        msg = soup.find("article").find("p").text
-        success = False
-        if msg.startswith("That's the right answer"):
-            success = True
-        return success, msg
 
     def get_stars(self, year, day):
         log.info("Get stars")
@@ -51,6 +94,14 @@ class AOC_Service:
         stars = self.parse_stars(resp.text)
         log.info(f"Got {stars} stars on this day")
         return stars
+
+    def parse_solution_result(self, text):
+        soup = BeautifulSoup(text, "html.parser")
+        msg = soup.find("article").find("p").text
+        success = False
+        if msg.startswith("That's the right answer"):
+            success = True
+        return success, msg
 
     def send_solution(self, year, day, part, answer):
         resp = self.aoc_session.post(

4f34cdae9ed539a06339dddd6e4a336db8d26edc
Miloslav Homer <miloslav.homer@gmail.com>
2024-12-17 09:03:37 +0100

    feat: 2024, day16

---
+++ tests/year2024/test_year2024_day16.py
@@ -0,0 +1,126 @@
+# test 2024-16
+
+import pytest
+
+from aoc.year2024.day16 import part1, part2
+
+simple_test = """###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############
+"""
+
+
+def test_part1():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "7036",
+            "ex": None,
+        },
+        {
+            "label": "simple2",
+            "input": {
+                "in_data": """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+""",
+                "test": True,
+            },
+            "output": "11048",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part1(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )
+
+
+def test_part2():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "45",
+            "ex": None,
+        },
+        {
+            "label": "simple2",
+            "input": {
+                "in_data": """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+""",
+                "test": True,
+            },
+            "output": "64",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part2(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )

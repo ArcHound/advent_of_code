520b17c1e758a635a0dd2735f3e38a45a28de0cd
Miloslav Homer <miloslav.homer@gmail.com>
2024-12-23 20:43:23 +0100

    fix: 2024 tests

--- tests/year2024/test_year2024_day13.py
+++ tests/year2024/test_year2024_day13.py
@@ -43,26 +43,3 @@ def test_part1():
             assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
                 case["label"], case["ex"], type(e)
             )
-
-
-def test_part2():
-    cases = [
-        {
-            "label": "small",
-            "input": {"in_data": simple_test},
-            "output": "part2 output 2024-13",
-            "ex": None,
-        },
-    ]
-    for case in cases:
-        try:
-            output = part2(**case["input"])
-            assert str(output) == str(
-                case["output"]
-            ), "case '{}', output: exp {}, got {}".format(
-                case["label"], case["output"], output
-            )
-        except Exception as e:
-            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
-                case["label"], case["ex"], type(e)
-            )

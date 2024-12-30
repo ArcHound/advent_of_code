acc9b104453b64f92260849b7d8b233b79b3dbd0
Miloslav Homer <miloslav.homer@gmail.com>
2024-12-30 13:18:12 +0100

    feat: 2022, day 12

---
+++ tests/year2022/test_year2022_day12.py
@@ -0,0 +1,64 @@
+# test 2022-12
+
+import pytest
+
+from aoc.year2022.day12 import part1, part2
+
+simple_test = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+"""
+
+
+def test_part1():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "31",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part1(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )
+
+
+def test_part2():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "29",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part2(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )

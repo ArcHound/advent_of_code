246d0636ff07edad6dda6c68692b10faf37757e6
Miloslav Homer <miloslav.homer@gmail.com>
2025-01-02 17:31:51 +0100

    feat: 2022, day 16 (ugh)

---
+++ tests/year2022/test_year2022_day16.py
@@ -0,0 +1,69 @@
+# test 2022-16
+
+import pytest
+
+from aoc.year2022.day16 import part1, part2
+
+simple_test = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+"""
+
+
+def test_part1():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "1651",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part1(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )
+
+
+def test_part2():
+    cases = [
+        {
+            "label": "simple",
+            "input": {
+                "in_data": simple_test,
+                "test": True,
+            },
+            "output": "1707",
+            "ex": None,
+        },
+    ]
+    for case in cases:
+        try:
+            output = part2(**case["input"])
+            assert str(output) == str(
+                case["output"]
+            ), "case '{}', output: exp {}, got {}".format(
+                case["label"], case["output"], output
+            )
+        except Exception as e:
+            assert type(e) == case["ex"], "case '{}', ex: exp {}, got {}".format(
+                case["label"], case["ex"], type(e)
+            )
